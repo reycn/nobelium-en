983718940155a80b527f07ed19e7e1ae46ba5ebb
reycn <oyrx@vip.qq.com>
2022-03-23 13:48:15 +0800

    create

--- components/Footer.js
+++ components/Footer.js
@@ -1,13 +1,13 @@
 /* eslint-disable indent */
 /* eslint-disable no-tabs */
 import BLOG from '@/blog.config'
-import Vercel from '@/components/Vercel'
+// import Vercel from '@/components/Vercel'
 const Footer = ({ fullWidth }) => {
-  const d = new Date()
-  const y = d.getFullYear()
-  const from = +BLOG.since
-  return (
-    <div
+	const d = new Date()
+	const y = d.getFullYear()
+	const from = +BLOG.since
+	return (
+		<div
 			className={`mt-6 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 transition-all ${!fullWidth
 				? 'max-w-2xl px-4'
 				: 'px-4 md:px-24'}`}
